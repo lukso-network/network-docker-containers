8bcdb5e1c68b16ef81b96e9dbef535bc6049b710
Sushil <sushilman@gmail.com>
2023-05-21 22:04:53 +0200

    Adding configuration for lighthouse as the consensus client with erigon as the execution, with all docker containers running in its own network

---
+++ validator_import_keys_lighthouse.sh
@@ -0,0 +1,13 @@
+export $(grep -v '^#' .env)
+docker run \
+  --rm \
+  --env-file=.env \
+  --volume $CONFIGS_VOLUME:/configs \
+  --volume $TMP_VOLUME:/tmp/secrets \
+  --volume $KEYSTORES_VOLUME:/keystores \
+  --volume $VALIDATOR_DATA_VOLUME:/validator_data sigp/lighthouse:latest-modern lighthouse account validator import \
+  --testnet-dir /configs \
+  --password-file /tmp/secrets/password.txt \
+  --reuse-password \
+  --directory /keystores \
+  --datadir /validator_data
